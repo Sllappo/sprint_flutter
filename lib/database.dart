4008c9aa938be1684785e56f7c0d7529d18fc331
Matthieu LAURENT <ml.laurent.matthieu@gmail.com>
2024-10-29 13:36:11 +0100

    del .env

--- lib/database.dart
+++ lib/database.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:mongo_dart/mongo_dart.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-void main( ){
-  print("bonjour");
+
+void main( ) async{
 Future<void> connectToDb() async {
-  //String? connectionString = dotenv.env['DATABASE_STRING'];
-  //var db = mdb.Db(connectionString!);
   var password = 'root';
   var encryptedPassword = Uri.encodeComponent(password);
 
@@ -13,9 +11,13 @@ Future<void> connectToDb() async {
   try {
     await db.open();
     print('Connexion établie');
-  } catch (e) {
-    print('Erreur lors de la connexion : $e');
+  }catch(e){
+    print('Connexion error $e');
   }
+
+  // Votre code ici
+
+  await db.close();
 }
 
 connectToDb();
--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future main() async {
-
-  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
