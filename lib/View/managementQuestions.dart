4062f5571b1a5688a3f9df6cf96f0242f5c073c1
Younes.Bouchouk <younes.78.alg@gmail.com>
2024-10-30 21:38:42 +0100

    Commentaire dans managementQuestions.dart

--- lib/View/managementQuestions.dart
+++ lib/View/managementQuestions.dart
@@ -2,12 +2,14 @@ import 'package:sprint_flutter/Controller/getAllQuizz.dart';
 import 'package:flutter/material.dart';
 import 'package:sprint_flutter/Model/quizz.dart';
 
+// Fonction pour récupérer les quizz dans la table quiz
 Future<List<Quiz>> fetchQuizz() async {
   List<Map<String, dynamic>> quizzListJson = await getAllQuizz();
   List<Quiz> quizList = quizzListJson.map((json) => Quiz.fromJson(json)).toList();
   print(quizList);
   return quizList;
 }
+
 
 class ManagementQuestions extends StatefulWidget {
   const ManagementQuestions({super.key});
@@ -16,28 +18,33 @@ class ManagementQuestions extends StatefulWidget {
   _ManagementQuestionsState createState() => _ManagementQuestionsState();
 }
 
-class _ManagementQuestionsState extends State<ManagementQuestions> {
-  String selectedCategory = "HTML";
 
-  void updateCategory(String category) {
+class _ManagementQuestionsState extends State<ManagementQuestions> {
+  String selectedCategory = "HTML"; // Stocker la catégorie de question à afficher (HTML par défaut)
+
+  void updateCategory(String category) { // State pour mettre à jour la catégorie à afficher
     setState(() {
       selectedCategory = category;
     });
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) { // Contenue de la page
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Admin - Gestions des questions'),
+        title: const Text('Gestions des questions'),
         backgroundColor: Colors.blue,
+        actions: [
+          IconButton(onPressed: ()=>{}, icon: Icon(Icons.add))
+        ],
       ),
+      
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            NavBar(selectedCategory, updateCategory),
-            Expanded(child: QuizCard(selectedCategory: selectedCategory)),
+            NavBar(selectedCategory, updateCategory), // Barre de navigation
+            Expanded(child: QuizCard(selectedCategory: selectedCategory)), // Liste des questions sous forme de Card
           ],
         ),
       ),
@@ -45,24 +52,23 @@ class _ManagementQuestionsState extends State<ManagementQuestions> {
   }
 }
 
+// Barre de navigation
 Widget NavBar(String selectedCategory, Function(String) updateCategory) {
-  final List<Map<String, dynamic>> buttons = [
-    {'label': 'HTML', 'value': 'HTML'},
-    {'label': 'CSS', 'value': 'CSS'},
-    {'label': 'Java', 'value': 'Java'},
-    {'label': 'Algo', 'value': 'Algo'},
+  // Tableau d'objet
+  final List<String> categories = [
+    'HTML', 'CSS', 'JAVA', 'ALGO'
   ];
 
   return Row(
     mainAxisAlignment: MainAxisAlignment.center,
-    children: buttons.map((button) {
+    children: categories.map((category) { // Boucle sur les categories
       return Padding(
         padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 10.0),
         child: ElevatedButton(
-          onPressed: () => updateCategory(button['value']),
-          child: Text(button['label']),
+          onPressed: () => updateCategory(category), // Changer la categorie selectionner
+          child: Text(category),
           style: ElevatedButton.styleFrom(
-            backgroundColor: selectedCategory == button['value'] ? Colors.blueAccent : null,
+            backgroundColor: selectedCategory == category ? Colors.blueAccent : null, // Background de couleur pour la catégorie selectionnée
           ),
         ),
       );
@@ -70,56 +76,45 @@ Widget NavBar(String selectedCategory, Function(String) updateCategory) {
   );
 }
 
-// Widget QuizCard
+// Card qui affiche une question et ses réponses
 class QuizCard extends StatelessWidget {
-  final String selectedCategory;
+  final String selectedCategory; // catégorie selectionnée
 
   const QuizCard({required this.selectedCategory});
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<Quiz>>(
-      future: fetchQuizz(),
+    return FutureBuilder<List<Quiz>>( // Attendre le fetch puis afficher
+      future: fetchQuizz(), // Récupérer les questions
       builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator());
-        } else if (snapshot.hasError) {
+        if (snapshot.connectionState == ConnectionState.waiting) { // En attendant la requête
+          return const Center(child: CircularProgressIndicator()); // icône de chargement
+        } else if (snapshot.hasError) { // Si erreur
           print("Erreur dans FutureBuilder: ${snapshot.error}");
-          return const Center(child: Text('Erreur lors du chargement du quiz'));
+          return const Center(child: Text('Erreur lors du chargement du quiz')); // Si erreur
         } else if (snapshot.hasData) {
-          // Filtrer les quizzes en fonction de la catégorie sélectionnée
+          // Filtrer les questions selon la catégorie selectionné
           final filteredQuizzes = snapshot.data!
               .where((quiz) => quiz.category == selectedCategory)
               .toList();
 
           return ListView(
-            children: filteredQuizzes.map((item) {
+            children: filteredQuizzes.map((item) { // Boucle sur toutes les questions filtrées
               return Card(
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        item.category,
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    ...item.questions.map((question) {
+                    ...item.questions.map((question) { // Boucle sur les questions de la catégorie
                       return Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 4.0),
+                        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text(
+                            Text( // Affiche la question
                               question.text,
-                              style: const TextStyle(fontSize: 18),
+                              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                             ),
-                            // Affichage des choix avec style conditionnel
-                            ...question.choices.asMap().entries.map((entry) {
+                            ...question.choices.asMap().entries.map((entry) { // Boucle sur les choix possible
                               final int index = entry.key;
                               final String choice = entry.value;
 
